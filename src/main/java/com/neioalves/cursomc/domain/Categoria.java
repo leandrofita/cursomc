539b661779c69bbbeece79db6fbbba714b025ebb
Leandro Fita <leandrocfita@gmail.com>
2022-02-13 18:44:10 -0300

    Atualização utilização do JSONIgnore

--- src/main/java/com/neioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/neioalves/cursomc/domain/Categoria.java
@@ -11,8 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Categoria implements Serializable {
 	
@@ -24,7 +22,7 @@ public class Categoria implements Serializable {
 	private String nome;
 	
 	/* Anotação feita no lado onde deverá ser exibido os objetos associados (os produtos da categoria) */
-	@JsonManagedReference
+	
 	/* Associação da categoria com o produto, uma categoria pode ter vários produtos */
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
--- src/main/java/com/neioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/neioalves/cursomc/domain/Pagamento.java
@@ -11,7 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.neioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -27,7 +27,7 @@ public abstract class Pagamento implements Serializable {
 	//Estado do pagamento precisa ser inteiro para seguir as adaptações do enum EstadoPagamento
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	//o pagamento irá utilizar o mesmo ID do pedido
 	@JoinColumn(name="pedido_id")
